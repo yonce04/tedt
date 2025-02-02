1e21ee7bc4f79a29907602a9b548f913b9907f84
Your Name <you@example.com>
2025-02-02 15:01:33 +0100

    adding

---
+++ super.py
@@ -0,0 +1,14 @@
+from imageai.Classification import ImageClassification
+import os
+ 
+exec_path = os.getcwd()
+ 
+prediction = ImageClassification()
+# SqueezeNet model also no longer exists, now the fastest is MobileNetV2
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(os.path.join(exec_path, 'mobilenet_v2-b0353104.pth'))
+prediction.loadModel()
+ 
+predctions, probabilities = prediction.classifyImage(os.path.join(exec_path,'house.jpg'), result_count=10)
+for eachPred, eachProb in zip(predctions, probabilities):
+    print(f'{eachPred} : {eachProb}')
